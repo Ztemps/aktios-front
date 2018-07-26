fb6e542233567f7128b9fb44cbdf71d086c6d552
Javi <javicasados@gmail.com>
2018-07-27 01:51:40 +0200

    Search refactor

--- src/shared/search/search.service.ts
+++ src/shared/search/search.service.ts
@@ -2,11 +2,39 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { API_URL } from '../../constants';
+import { Person } from '../../app/models/person.model';
 
 @Injectable()
 export class SearchService {
-
+    public staff: any;
+    public results: any;
+    public totalResults: [Person];
     constructor(public http: HttpClient) { }
+
+    match() {
+      this.getStaff().subscribe((res: Person) => {
+        this.staff = res;
+        this.totalResults = this.staff;
+        this.getDataSource().subscribe(resp => {
+          this.results = resp;
+          this.staff.forEach((staff, i) => {
+            this.results.sex.forEach(data => {
+              if (staff.sex === data.key) {
+                this.staff[i].sex = data.description;
+              }
+            });
+            this.results.country.forEach(data => {
+              if (staff['country-id'] === data.id) {
+                this.staff[i].phone = `+${data.prefix} ${this.staff[i].phone}`;
+                this.staff[i].country = data.description;
+              }
+            });
+          });
+        });
+      });
+
+      return this.totalResults;
+    }
 
     getDataSource() {
         return this.http.get(`${API_URL}/data`)
@@ -25,4 +53,5 @@ export class SearchService {
           })
       );
   }
+
 }
